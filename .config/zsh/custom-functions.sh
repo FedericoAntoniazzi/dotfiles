ba45b1b9da91b2fd90c19f8c81ed613fca5dbbd6
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2021-07-26 21:56:38 +0200

    feat: experiment with fzf for searching packages

--- .config/zsh/custom-functions.sh
+++ .config/zsh/custom-functions.sh
@@ -1,3 +1,21 @@
 mkcdir () {
 	mkdir -p $1 && cd $_
 }
+
+pacsi () {
+	if [[ $# -eq 0 ]]; then
+		echo "No argument provided"
+		echo "Usage: pacsi <package>"
+		return 1
+	fi
+	pacman -Si $(pacman -Ssq $@ | fzf)
+}
+
+pacqi () {
+	if [[ $# -eq 0 ]]; then
+		echo "No argument provided"
+		echo "Usage: pacqi <package>"
+		return 1
+	fi
+	pacman -Qi $(pacman -Qsq $@ | fzf)
+}

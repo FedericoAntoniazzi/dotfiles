fce96f780e8982be580a781088fa3e4c300fe965
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2021-08-30 17:03:04 +0200

    multi monitor, wallpaper, x resources

--- .config/dotscripts/multimonitor.sh
+++ .config/dotscripts/multimonitor.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if xrandr -q | grep 'HDMI-1 connected' ; then
+autorandr --load $(autorandr --detected)
 
-	xrandr --output eDP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+if xrandr -q | grep 'HDMI-1 connected primary' ; then
 	
 	bspc monitor HDMI-1 -d 1 2 3 4 5 6 7
 	bspc monitor eDP-1 -d 8 9 0

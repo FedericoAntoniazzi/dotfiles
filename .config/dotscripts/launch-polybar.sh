58136abab8ff1e8e721e688fa27a6fb70a44452a
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2022-07-05 22:45:19 +0200

    feat: reset for new setup from scratch using wayland

--- .config/dotscripts/launch-polybar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Terminate already running bar instances
-killall -q polybar
-
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar main &
-done
-
